9a02d34c0285fcc23b3c6cd0691392260ae40935
kashyapSheladiya <kashyap27041993@gmail.com>
2021-03-19 14:10:33 +0100

    using view engine ejs

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen request
 app.listen(3000);
 
-// app.get('/', (req, res) => {
-//   res.send('<p>hello</p>');   //automatically sets header and status code
-// })
-
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {root: __dirname});
+  res.render('index');
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', {root: __dirname});
+  res.render('about');
 })
 
 // redirects
@@ -25,5 +24,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 - always at a bottom, similar to default if all above routes doesn't match
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', {root: __dirname});  //necessary to pass status_code. else it will use 200 success.
+  res.status(404).render('404');  //necessary to pass status_code. else it will use 200 success.
 })
