418f9280c017f836c860b357685055adf927753e
kashyapSheladiya <kashyap27041993@gmail.com>
2021-03-19 14:41:46 +0100

    using partials, add css, handle data with routes

--- app.js
+++ app.js
@@ -10,18 +10,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 })
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', { title: 'About' });
 })
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create');
+  res.render('create', { title: 'Create a New Blog' });
 })
 
 // 404 - always at a bottom, similar to default if all above routes doesn't match
 app.use((req, res) => {
-  res.status(404).render('404');  //necessary to pass status_code. else it will use 200 success.
+  res.status(404).render('404', { title: '404' });  //necessary to pass status_code. else it will use 200 success.
 })
