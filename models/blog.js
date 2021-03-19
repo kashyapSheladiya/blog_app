04c47c4e0eaaa2dff5b3c5723c3d9a064eeeb0e2
kashyapSheladiya <kashyap27041993@gmail.com>
2021-03-19 18:10:51 +0100

    create Blog model and schema using mongoose

---
+++ models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// constructor function to create blog schema
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  snippet: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true
+  },
+}, { timestamps: true });
+
+const Blog = mongoose.model('Blog', blogSchema);
+module.exports = Blog;
